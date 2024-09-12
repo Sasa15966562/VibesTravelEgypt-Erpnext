2c7fc16263383b96b3a0d53deebaf176636e86c1
sasa15966562 <mustafanazieer@gmail.com>
2024-09-12 16:50:26 +0300

    Added report for customer/supplier transactions with multi-currency support

---
+++ custom_reports/customer_and_supplier_transactions_with_multi_currency/__init__.py
@@ -0,0 +1,2 @@
+# __init__.py
+from __future__ import unicode_literals
---
+++ custom_reports/customer_and_supplier_transactions_with_multi_currency/customer_and_supplier_transactions_with_multi_currency.py
@@ -0,0 +1,86 @@
+import frappe
+from frappe import _
+
+def execute(filters=None):
+    columns = get_columns()
+    data = get_data(filters)
+    return columns, data
+
+def get_columns():
+    return [
+        {
+            "fieldname": "party",
+            "label": _("Party"),
+            "fieldtype": "Link",
+            "options": "Party",
+            "width": 200
+        },
+        {
+            "fieldname": "party_type",
+            "label": _("Party Type"),
+            "fieldtype": "Data",
+            "width": 150
+        },
+        {
+            "fieldname": "currency",
+            "label": _("Currency"),
+            "fieldtype": "Link",
+            "options": "Currency",
+            "width": 100
+        },
+        {
+            "fieldname": "debit",
+            "label": _("Debit"),
+            "fieldtype": "Currency",
+            "options": "currency",
+            "width": 150
+        },
+        {
+            "fieldname": "credit",
+            "label": _("Credit"),
+            "fieldtype": "Currency",
+            "options": "currency",
+            "width": 150
+        },
+        {
+            "fieldname": "balance",
+            "label": _("Balance"),
+            "fieldtype": "Currency",
+            "options": "currency",
+            "width": 150
+        },
+        {
+            "fieldname": "posting_date",
+            "label": _("Posting Date"),
+            "fieldtype": "Date",
+            "width": 120
+        }
+    ]
+
+def get_data(filters):
+    data = []
+    
+    # Fetching both supplier and customer GL entries
+    gl_entries = frappe.get_all(
+        "GL Entry",
+        filters={
+            "party_type": ["in", ["Customer", "Supplier"]],
+            "posting_date": ["between", [filters.get("from_date"), filters.get("to_date")]]
+        },
+        fields=["party", "party_type", "account_currency", "debit", "credit", "posting_date"]
+    )
+    
+    # Processing data to include balance calculations
+    for entry in gl_entries:
+        balance = entry.debit - entry.credit
+        data.append({
+            "party": entry.party,
+            "party_type": entry.party_type,
+            "currency": entry.account_currency,
+            "debit": entry.debit,
+            "credit": entry.credit,
+            "balance": balance,
+            "posting_date": entry.posting_date
+        })
+    
+    return data
